b9a1be66643738917ec6610455bd3fb058ebd7aa
nnbzh <nurasyl.bbn@gmail.com>
2021-02-07 23:59:01 +0600

    Initial migrations added

---
+++ database/migrations/2021_02_07_153205_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->integer('amount');
+            $table->integer('calories');
+            $table->integer('weight');
+            $table->decimal('price');
+            $table->string('img_src');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
