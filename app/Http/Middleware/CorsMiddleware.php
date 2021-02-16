fab8837cb02ff4b8f5e8632afb668e348940231a
nnbzh <nurasyl.bbn@gmail.com>
2021-02-17 00:05:19 +0600

    CORS policy fixed

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -14,12 +14,11 @@ class CorsMiddleware
     {
         $headers = [
             'Access-Control-Allow-Origin'      => '*',
-            'Access-Control-Allow-Methods'     => 'POST, GET,
-        OPTIONS,PUT,DELETE',
+            'Access-Control-Allow-Methods'     => 'POST, GET, OPTIONS, PUT, DELETE',
             'Access-Control-Allow-Credentials' => 'true',
             'Access-Control-Max-Age'           => '86400',
             'Access-Control-Allow-Headers'     => 'Content-Type, Authorization,
-        X-Requested-With'
+            X-Requested-With'
         ];
 
         if ($request->isMethod('OPTIONS'))
